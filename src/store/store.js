d11c1769c607da79f6ea57e0406838967724415a
Mostafa Mahran <mfamahran@gmail.com>
2019-06-28 17:08:57 +0200

    Update

--- src/store/store.js
+++ src/store/store.js
@@ -19,7 +19,6 @@ import actions from "./actions"
 Vue.use(Vuex)
 
 import moduleAuth from './auth/moduleAuth.js'
-import moduleECommerce from './eCommerce/moduleECommerce.js'
 
 
 export default new Vuex.Store({
@@ -29,7 +28,6 @@ export default new Vuex.Store({
     actions,
     modules: {
         auth: moduleAuth,
-        eCommerce: moduleECommerce,
     },
     strict: process.env.NODE_ENV !== 'production'
 })
